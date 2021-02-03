89ec35f6eaf35b46af7b0314142d770551f8400f
Szymon Socha <szymon.socha@venturedevs.com>
2021-02-03 18:05:06 +0100

    Added new IEnumerable extension to convert to DataTable.

--- Indico.BluePrism.Connector.Tests/Helpers/DataTableExtensionsTests.cs
+++ Indico.BluePrism.Connector.Tests/Helpers/DataTableExtensionsTests.cs
@@ -3,6 +3,8 @@ using Indico.BluePrism.Connector.Helpers;
 using FluentAssertions;
 using System.Collections.Generic;
 using System.Linq;
+using Indico.Entity;
+using Indico.Types;
 
 namespace Indico.BluePrism.Connector.Tests.Helpers
 {
@@ -23,6 +25,48 @@ namespace Indico.BluePrism.Connector.Tests.Helpers
             //Assert
             dataTable.Should().NotBeNull();
             dataTable.Rows.Should().HaveCount(elementCount);
+        }
+
+        [TestCase(1, SubmissionStatus.COMPLETE, "test")]
+        [TestCase(2, SubmissionStatus.FAILED, "test2")]
+        public void ToDetailedDataTable_ShouldReturnValidObject(int id, SubmissionStatus status, string inputFile)
+        {
+            //Arrange
+            var testObject = new Submission
+            {
+                Id = id,
+                Status = status,
+                InputFile = inputFile
+            };
+
+            //Act
+            var dataTable = new List<Submission>() { testObject }.ToDetailedDataTable();
+
+            //Assert
+            dataTable.Should().NotBeNull();
+            dataTable.Rows[0][nameof(testObject.Id)].Should().Equals(id);
+            dataTable.Rows[0][nameof(testObject.Status)].Should().Equals(status);
+            dataTable.Rows[0][nameof(testObject.InputFile)].Should().Equals(inputFile);
+        }
+
+        [TestCase(1)]
+        [TestCase(8)]
+        public void ToDetailedDataTable_ShouldReturnAllElements(int count)
+        {
+            //Arrange
+            var list = new List<Submission>();
+
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(new Submission { Id = i });
+            }
+            
+            //Act
+            var dataTable = list.ToDetailedDataTable();
+
+            //Assert
+            dataTable.Should().NotBeNull();
+            dataTable.Rows.Count.Should().Equals(count);
         }
     }
 }
--- Indico.BluePrism.Connector/Helpers/EnumerableExtensions.cs
+++ Indico.BluePrism.Connector/Helpers/EnumerableExtensions.cs
@@ -18,5 +18,30 @@ namespace Indico.BluePrism.Connector.Helpers
 
             return result;
         }
+
+        public static DataTable ToDetailedDataTable<T>(this IEnumerable<T> items)
+        {
+            var tb = new DataTable(typeof(T).Name);
+
+            var props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var prop in props)
+            {
+                tb.Columns.Add(prop.Name, prop.PropertyType);
+            }
+
+            foreach (var item in items)
+            {
+                var values = new object[props.Length];
+                for (var i = 0; i < props.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item, null);
+                }
+
+                tb.Rows.Add(values);
+            }
+
+            return tb;
+        }
     }
 }
