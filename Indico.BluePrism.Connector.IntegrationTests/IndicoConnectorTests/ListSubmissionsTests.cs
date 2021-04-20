274ed2c0262ecee9d7ad3ef462804913ce8d9e2c
Szymon Socha <szymon.socha@venturedevs.com>
2021-04-20 09:16:33 +0200

    Fixed formatting.

--- Indico.BluePrism.Connector.IntegrationTests/IndicoConnectorTests/ListSubmissionsTests.cs
+++ Indico.BluePrism.Connector.IntegrationTests/IndicoConnectorTests/ListSubmissionsTests.cs
@@ -14,7 +14,7 @@ namespace Indico.BluePrism.Connector.IntegrationTests.IndicoConnectorTests
             var connector = new IndicoConnectorBuilder().Build();
             const string expectedExceptionMessage =
                 @"1 : http://sunbow:5000/api/submission?workflow_ids=0&limit=1&filters=%7B%7D: {""code"": 403, ""content"": {}, ""error_class"": ""ServiceException"", ""error_type"": ""ForbiddenAccess"", ""message"": ""This user does not have access to this resource""}";
-            connector.Invoking(c => c.ListSubmissions(null, new decimal[] {0}.ToDataTable(), null, null, null, 1))
+            connector.Invoking(c => c.ListSubmissions(null, new decimal[] { 0 }.ToDataTable(), null, null, null, 1))
                 .Should()
                 .Throw<GraphQLException>()
                 .WithMessage(expectedExceptionMessage);
